a90272f3549fbfa9ad290ed274f24265c064420e
SungYoonCho <sungyoon606@naver.com>
2020-09-21 16:44:22 +0900

    Flow control practice

---
+++ JAVA1_opentutorial/java-flow-control/BooleanApp.java
@@ -0,0 +1,14 @@
+
+public class BooleanApp {
+
+	public static void main(String[] args) {
+		System.out.println(true);
+		System.out.println(false);
+		
+		String foo = "Hello World";
+		System.out.println(foo.contains("World"));
+		System.out.println(foo.contains("sy"));
+
+	}
+
+}
