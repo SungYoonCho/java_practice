c798a73cef154d8f113bd66f63fcc3ad6dc3d8b4
SungYoonCho <sungyoon606@naver.com>
2020-09-21 15:38:28 +0900

    Opentutorial practice for JAVA

---
+++ JAVA1_opentutorial/data_and_operation/Letter.java
@@ -0,0 +1,12 @@
+
+public class Letter {
+
+	public static void main(String[] args) {
+		String name = "sy";
+		System.out.println("Hello, "+name+"..."+name+"... sy ... bye");
+		
+		double VAT = 10.0;
+		System.out.println(VAT);
+	}
+
+}
---
+++ JAVA1_opentutorial/data_and_operation/StringApp.java
@@ -0,0 +1,19 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld"); // String
+		System.out.println('H'); // Character
+		System.out.println("H"); // no error but... H is called "String"
+		
+		System.out.println("Hello "
+				+ "World"); 
+		
+		// new line
+		System.out.println("Hello \nWorld"); 
+		
+		//escape
+		System.out.println("Hello \"World\""); // Hello "World"
+	}
+
+}
